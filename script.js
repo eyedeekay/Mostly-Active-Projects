4c03a5c2c2222fbbf8e96d187bcd4f67643a1908
idk <hankhill19580@gmail.com>
2023-01-19 02:09:35 +0000

    page generation update for: 2023-01-19 02:09:35.542610126 +0000 UTC m=+2.565227480

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 import GitHub from 'github-api';
 
-// basic auth
-var gh = new GitHub();
-
-var me = gh.getUser("eyedeekay"); // no user specified defaults to the user for whom credentials were provided
-me.listNotifications(function(err, notifications) {
-   // do some stuff
+// unauthenticated client
+const gh = new GitHub();
+let gist = gh.getGist(); // not a gist yet
+gist.create({
+public: true,
+description: 'My first gist',
+files: {
+    "file1.txt": {
+    content: "Aren't gists great!"
+    }
+}
+}).then(function({data}) {
+// Promises!
+let createdGist = data;
+return gist.read();
+}).then(function({data}) {
+let retrievedGist = data;
+// do interesting things
 });
