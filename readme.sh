e4cf6e81b7600cae27a99a1680e65a9fb5babef6
idk <hankhill19580@gmail.com>
2023-01-19 01:46:12 +0000

    page generation update for: 2023-01-19 01:46:11.944356404 +0000 UTC m=+2.949802731

--- readme.sh
+++ readme.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env sh
 
-rm -f README.tmp.md
+rm -f README.md
 
-echo "idk's mostly active projects" | tee README.tmp.md
-echo "============================" | tee -a README.tmp.md
-
-cat desc | tee -a README.tmp.md
-
+echo "idk's mostly active projects" | tee README.md
+echo "============================" | tee -a README.md
+echo "" | tee -a README.md
+cat desc | tee -a README.md
+echo "" | tee -a README.md
 for repo in $(cat config.rc); do
-    echo -n "* [$repo]" | sed 's|https://github.com/eyedeekay/||g' | tr '-' ' ' | tr '_' ' ' | tee -a README.tmp.md
-    echo "($repo)" | tee -a README.tmp.md
+    echo -n "* [$repo]" | sed 's|https://github.com/eyedeekay/||g' | tr '-' ' ' | tr '_' ' ' | tee -a README.md
+    echo "($repo)" | tee -a README.md
 done
 
 edgar -script Github.bundle.min.js,script.js
