6bd8e95befd956db3c3130833dde52120edcefa4
idk <hankhill19580@gmail.com>
2023-01-19 01:36:59 +0000

    work on script

--- readme.sh
+++ readme.sh
@@ -1,2 +1,13 @@
 #! /usr/bin/env sh
 
+rm -f README.tmp.md
+
+echo "idk's mostly active projects" | tee README.tmp.md
+echo "============================" | tee -a README.tmp.md
+
+cat desc | tee -a README.tmp.md
+
+for repo in $(cat config.rc); do
+    echo -n "* [$repo]" | sed 's|https://github.com/eyedeekay/||g' | tr '-' ' ' | tr '_' ' ' | tee -a README.tmp.md
+    echo "($repo)" | tee -a README.tmp.md
+done
